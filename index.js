86b34aa6e500247b3b68f4c986d2297d209bab8c
ChronoKross <89613945+ChronoKross@users.noreply.github.com>
2023-01-23 01:26:24 -0500

    added port variable
    removed hardcoded 500 port for production

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const postRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const port = process.env.PORT || 5000; //test
 
 dotenv.config();
 app.use(express.json());
@@ -37,6 +38,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoriesRoute);
 
-app.listen("5000", () => {
+app.listen(port, () => {
   console.log("Backend is running.");
 });
